a6374e73414bd76f8c2deb5ecf5088ea401d8e59
jykim1187 <rifkehtm862@gmail.com>
2024-11-28 14:09:40 +0900

    viah모드 테스트 1)수정사항 1 2)수정사항 2 3)수정사항 3

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -4,3 +4,11 @@ git status
 # . :모든 수정 및 추가사항을 의미
 git add .
 git add 특정파일(경로포함)
+
+# commit을 통해 메세지타이틀과 메시지내용을 커밋이력으로 생성
+git commit -m "메세지 타이틀" -m "메세지 내용"
+
+# git commit만 하고 엔터 실행시 vi모드
+# 첫줄 : 타이틀, 두번째 줄부터 contents
+
+git commit
