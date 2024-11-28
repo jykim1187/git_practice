c229834e29ee32592f70eb488bbf74e70835b9f3
jykim1187 <rifkehtm862@gmail.com>
2024-11-28 17:28:24 +0900

    11/28 final

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -74,8 +74,5 @@ git branch -D 브랜치명
 # git fetch : 원격에 변경사항을 local에 가져오되, 병합은 하지 않는 것
 # 비교 : git pull origin main vs git fetch origin main
 
-
-
-
 # 원격에서 삭제된 브랜치 로컬에 반영(orgin/feat1)
 git fetch --all --prune
